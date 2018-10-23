c4909e8e090645c4f7d0df12da1fa93a3ecbc03a
tomoki69386 <tomoki_69386@yahoo.co.jp>
2018-10-24 03:20:39 +0900

    Initial Commit

---
+++ ARPlus/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ARPlus
+//
+//  Created by 築山朋紀 on 2018/10/24.
+//  Copyright © 2018 築山朋紀. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
