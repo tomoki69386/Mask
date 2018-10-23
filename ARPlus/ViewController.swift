4bb0c1da5862f24e5548855ab81e7ec2d77e0d71
tomoki69386 <tomoki_69386@yahoo.co.jp>
2018-10-24 03:49:38 +0900

    Update

--- ARPlus/ViewController.swift
+++ ARPlus/ViewController.swift
@@ -9,10 +9,29 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    var faceTracker: FaceTracker? = nil
+    @IBOutlet var cameraView :UIView!
+    var rectView = UIView()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        //四角い枠を用意しておく
+        self.rectView.layer.borderWidth = 3
+        rectView.layer.borderColor = UIColor.yellow.cgColor
+        self.view.addSubview(self.rectView)
+        faceTracker = FaceTracker(view: self.cameraView, findface:{arr in
+            //一番の顔だけ使う
+            let rect = arr[0]
+            
+            //四角い枠を顔の位置に移動する
+            self.rectView.frame = rect
+        })
     }
 
 
